ed61408ef09652cddb5891cb53783c92cbfdb10f
jin9173 <jinhappier2@gmail.com>
2019-12-20 20:56:56 +0900

    templates/post_list.html을 추가하고, 해당 내용을 post_list view에서 돌려주도록 함

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,17 @@
+import os
+
 from django.http import HttpResponse
 
 
 def post_list(request):
-    return HttpResponse('Post List!')
+    cur_file_path = os.path.abspath(__file__)
+    blog_dir_path = os.path.dirname(cur_file_path)
+    root_dir_path = os.path.dirname(blog_dir_path)
+    templates_dir_path = os.path.join(root_dir_path, 'templates')
+    post_list_html_path = os.path.join(templates_dir_path, 'post_list.html')
+
+    f = open(post_list_html_path, 'rt')
+    html = f.read()
+    f.close()
+
+    return HttpResponse(html)

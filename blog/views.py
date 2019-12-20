07f9d8b84da2f508e41bab6c888ade6131c7b71e
jin9173 <jinhappier2@gmail.com>
2019-12-20 23:56:27 +0900

    django shorcut함수인 render함수 사용

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,8 @@
 import os
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def post_list(request):
-    cur_file_path = os.path.abspath(__file__)
-    blog_dir_path = os.path.dirname(cur_file_path)
-    root_dir_path = os.path.dirname(blog_dir_path)
-    templates_dir_path = os.path.join(root_dir_path, 'templates')
-    post_list_html_path = os.path.join(templates_dir_path, 'post_list.html')
-
-    f = open(post_list_html_path, 'rt')
-    html = f.read()
-    f.close()
-
-    return HttpResponse(html)
+    return render(request, 'post_list.html')

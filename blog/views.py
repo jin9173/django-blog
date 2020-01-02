cf70816d4d97411dd5e2d713dc851db9c829027a
jin9173 <jinhappier2@gmail.com>
2020-01-02 11:56:58 +0900

    post_detail.html에 스타일 적용

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,4 @@
-import os
-
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
@@ -12,3 +9,11 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'post_list.html', context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'post_detail.html', context)
